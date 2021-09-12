45b7be968bcfbefe7f85c793da6d2dd2c96a1331
Frosya Bulochkna <konyakova.elena@gmail.com>
2021-09-12 15:40:40 +0300

    StudentInfo

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Lena Kachanova");
+        System.out.println("12.04.1993");
+    }
+}
