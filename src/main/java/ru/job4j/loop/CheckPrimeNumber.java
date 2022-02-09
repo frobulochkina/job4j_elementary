24dbfd2a7709f65a9fc70208bb72c647fc3e2e60
frobulochkina <konyakova.elena@gmail.com>
2022-02-09 15:52:47 +0300

    fixed CheckPrimeNumber

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -4,13 +4,16 @@ public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean prime = true;
         int     count = 0;
+        if (number == 1) {
+            return false;
+        }
         for (int i = 1; i <= number; i++) {
             if ((number % i) == 0) {
                 count += 1;
             }
             if (count > 2) {
-              prime = false;
-              break;
+                prime = false;
+                break;
             }
         }
         return prime;
