7e4ab053abcf798b53c2af81d2b2e5d226f2155d
frobulochkina <konyakova.elena@gmail.com>
2022-02-09 17:52:52 +0300

    array/ArrayDefinition

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,8 +5,16 @@ public class ArrayDefinition {
         short[]     ages = new short[10];
         String[]    surnames = new String[100500];
         float[]     prices = new float[40];
+        String[]    names = new String[4];
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+        names[0] = "Squidward";
+        names[1] = "Garry";
+        names[2] = "Spanch Bob";
+        names[3] = "Patrick";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
