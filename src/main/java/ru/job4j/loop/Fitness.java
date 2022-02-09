a586ee58a99484e6e4c4e4469249e0c06e830424
frobulochkina <konyakova.elena@gmail.com>
2022-02-09 14:26:51 +0300

    add loop/Fitness

---
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        for (; ivan <= nik; month++) {
+            ivan *= 3;
+            nik *= 2;
+        }
+        return month;
+    }
+
+    public static void  main(String[] args) {
+        System.out.println(calc(90, 100));
+    }
+}
