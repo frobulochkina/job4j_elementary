a63742e4750cb0a7e30edd28c0dbaacc7d12aefd
frobulochkina <konyakova.elena@gmail.com>
2022-02-09 16:44:39 +0300

    add loop/Mortgage

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
+        percent = percent / 100;
+        amount = amount + (amount * percent);
+        while (amount > 0) {
+            amount = (amount - salary) * percent;
+            year++;
+        }
+        return year;
+    }
+}
