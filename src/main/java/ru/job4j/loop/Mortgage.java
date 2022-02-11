89b832fe11a248e9bf5830c152e4fb3ff173437d
frobulochkina <konyakova.elena@gmail.com>
2022-02-11 13:34:41 +0300

    fixed Mortgage

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,10 +3,9 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        percent = percent / 100;
-        amount = amount + (amount * percent);
+        
         while (amount > 0) {
-            amount = (amount - salary) * percent;
+            amount += amount * percent / 100 - salary;
             year++;
         }
         return year;
