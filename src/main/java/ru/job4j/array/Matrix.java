7cc819449c484278cbe00b6b2edb2af7ed9d7b2d
frobulochkina <konyakova.elena@gmail.com>
2022-02-14 21:18:14 +0300

    fixed Matrix

--- src/main/java/ru/job4j/array/Matrix.java
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -5,7 +5,7 @@ public class Matrix {
         int[][] arr = new int[size][size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                arr[i][j] = (i+1) * (j+1);
+                arr[i][j] = (i + 1) * (j + 1);
             }
         }
         return arr;
