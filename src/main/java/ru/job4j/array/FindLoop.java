5dcdc48179e98d1e25d7525c6bcf564a860b8626
frobulochkina <konyakova.elena@gmail.com>
2022-02-15 21:33:09 +0300

    fixed FindLoop

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -3,9 +3,10 @@ package ru.job4j.array;
 public class FindLoop {
     public static int indexOf(int[] data, int el) {
         int rst = -1;
-        for (int i = 0; i < data.length - 1; i++) {
+        for (int i = 0; i < data.length; i++) {
             if (data[i] == el) {
                 rst = i;
+                break;
             }
         }
         return rst;
@@ -16,6 +17,7 @@ public class FindLoop {
         for (int i = start; i <= finish; i++) {
             if (data[i] == el) {
                 rst = i;
+                break;
             }
         }
         return rst;
